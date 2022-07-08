6ac6d975c8d0176335066a945df6d66410e45910
Vicyn <59997168+Oogarchenko@users.noreply.github.com>
2022-07-08 14:20:28 +0300

    Link styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <Navbar bg="primary" variant="dark">
           <Container>
             <Nav className="me-auto">
-              <Nav.Link href="/">Все котики</Nav.Link>
-              <Nav.Link href="/favourite">Любимые котики</Nav.Link>
+                <Link to="/" className="text-white p-2 text-decoration-none">Все котики</Link>
+                <Link to="/favourite" className="text-white p-2 text-decoration-none">Любимые котики</Link>
             </Nav>
           </Container>
         </Navbar>           
